be25fe3d3928135e6f1042575d982c05854e9373
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-21 18:06:43 +0300

    feat(pin13): add basic LED blinking functionality

--- pin13_button/pin13_button.ino
+++ pin13_button/pin13_button.ino
@@ -1,10 +1,12 @@
 // mega 2560
 #define BUTTON_PIN 2
+#define LED_PIN 13
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
@@ -13,10 +15,11 @@ void loop()
   if (digitalRead(BUTTON_PIN) == HIGH)
   {
     Serial.println("Button pressed");
+    digitalWrite(LED_PIN, HIGH);
   }
   else
   {
     Serial.println("Button not pressed");
+    digitalWrite(LED_PIN, LOW);
   }
-  delay(100);
 }
