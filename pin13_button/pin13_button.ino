60908ed19df7aa3d940677528acdbfe4e3ce4c87
Alexander_Ovchar <azalexxx02@gmail.com>
2025-05-21 18:57:36 +0300

    feat(potentiometr): add potentiometer reading functionality

--- pin13_button/pin13_button.ino
+++ pin13_button/pin13_button.ino
@@ -4,22 +4,18 @@
 
 void setup()
 {
-  Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  // Serial.println(diitalRead(BUTTON_PIN));
   if (digitalRead(BUTTON_PIN) == HIGH)
   {
-    Serial.println("Button pressed");
     digitalWrite(LED_PIN, HIGH);
   }
   else
   {
-    Serial.println("Button not pressed");
     digitalWrite(LED_PIN, LOW);
   }
 }
