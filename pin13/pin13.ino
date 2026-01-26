50d091a8c66043b6eb64728529e63587ea03c778
TraceOfHumanity <azalexxx02@gmail.com>
2026-01-26 23:46:10 +0200

    feat(pin13): enhance LED control with serial output for state changes

--- pin13/pin13.ino
+++ pin13/pin13.ino
@@ -3,12 +3,15 @@
 void setup()
 {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   digitalWrite(13, HIGH);
+  Serial.println("LED is on");
   delay(500);
   digitalWrite(13, LOW);
+  Serial.println("LED is off");
   delay(500);
 }
